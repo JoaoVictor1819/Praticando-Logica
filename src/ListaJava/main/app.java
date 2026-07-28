8cfd329663947bb346ad9319d7fe890c93c33da3
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 15:19:17 -0300

    mais uma linha

--- src/ListaJava/main/app.java
+++ src/ListaJava/main/app.java
@@ -9,13 +9,15 @@ public class app {
 
         Pessoa pessoa = new Pessoa("Victor", 19, "Engenhero de Software");
         Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola");
+        Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto");
 
 
         ArrayList<Pessoa> pessoas = new ArrayList<>();
         pessoas.add(pessoa);
         pessoas.add(pessoa1);
+        pessoas.add(pessoa2);
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println(pessoas.get(i));
         }
 
