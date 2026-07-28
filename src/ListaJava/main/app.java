547a4fd75e03b81e4f4d08861e19f64c684f0f3f
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 15:36:18 -0300

    adiconando novos dados para lista

--- src/ListaJava/main/app.java
+++ src/ListaJava/main/app.java
@@ -10,14 +10,18 @@ public class app {
         Pessoa pessoa = new Pessoa("Victor", 19, "Engenhero de Software");
         Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola");
         Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto");
+        Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico");
+        Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada");
 
 
         ArrayList<Pessoa> pessoas = new ArrayList<>();
         pessoas.add(pessoa);
         pessoas.add(pessoa1);
         pessoas.add(pessoa2);
+        pessoas.add(pessoa3);
+        pessoas.add(pessoa4);
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println(pessoas.get(i));
         }
 
