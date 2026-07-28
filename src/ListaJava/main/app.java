e125d5081910742a775e0eba592126b1fbd6a2c3
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 15:41:44 -0300

    adicionado mais um dado na lista

--- src/ListaJava/main/app.java
+++ src/ListaJava/main/app.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
 public class app {
     static void main(String[] args) {
 
-        Pessoa pessoa = new Pessoa("Victor", 19, "Engenhero de Software");
+        Pessoa pessoa = new Pessoa("Victor", 19, "Estagiario de engenharia de software");
         Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola");
         Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto");
         Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico");
         Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada");
+        Pessoa pessoa5 = new Pessoa("Lucas", 23, "Estagiario de direito");
 
 
         ArrayList<Pessoa> pessoas = new ArrayList<>();
@@ -20,8 +21,9 @@ public class app {
         pessoas.add(pessoa2);
         pessoas.add(pessoa3);
         pessoas.add(pessoa4);
+        pessoas.add(pessoa5);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             System.out.println(pessoas.get(i));
         }
 
