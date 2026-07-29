05a5f8e319f82f15826bc7ed9fe15a2af44ea4a6
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 21:30:18 -0300

    adicionado salario em pessoa

--- src/ListaJava/dominio/Pessoa.java
+++ src/ListaJava/dominio/Pessoa.java
@@ -4,13 +4,16 @@ public class Pessoa {
     public String nome;
     public int idade;
     public String trabalho;
+    private double salario;
 
 
-    public Pessoa(String nome, int idade, String trabalho) {
+    public Pessoa(String nome, int idade, String trabalho, double salario) {
         this.nome = nome;
         this.idade = idade;
         this.trabalho = trabalho;
+        this.salario = salario;
     }
+
 
     @Override
     public String toString() {
@@ -18,6 +21,7 @@ public class Pessoa {
                 "nome='" + nome + '\'' +
                 ", idade=" + idade +
                 ", trabalho='" + trabalho + '\'' +
+                ", salario=" + salario +
                 '}';
     }
 }
--- src/ListaJava/main/app.java
+++ src/ListaJava/main/app.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 public class app {
     static void main(String[] args) {
 
-        Pessoa pessoa = new Pessoa("Victor", 19, "Estagiario de engenharia de software");
-        Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola");
-        Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto");
-        Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico");
-        Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada");
-        Pessoa pessoa5 = new Pessoa("Lucas", 23, "Estagiario de direito");
+        Pessoa pessoa = new Pessoa("Victor", 19, "Estagiario de engenharia de software", 12323);
+        Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola",31231);
+        Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto",3123123);
+        Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico",31231);
+        Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada",534534);
+        Pessoa pessoa5 = new Pessoa("Lucas", 23, "Estagiario de direito",534535);
 
 
         ArrayList<Pessoa> pessoas = new ArrayList<>();
