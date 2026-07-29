fd0649919acb0a56d7f3c313d37d77a4ea24e544
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 21:39:44 -0300

    refazendo

--- src/ListaJava/main/app.java
+++ src/ListaJava/main/app.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 public class app {
     static void main(String[] args) {
 
-        Pessoa pessoa = new Pessoa("Victor", 19, "Estagiario de engenharia de software", "1313213-21");
-        Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola", "7567577567-75");
+        Pessoa pessoa = new Pessoa("Victor", 19, "Estagiario de engenharia de software", );
+        Pessoa pessoa1 = new Pessoa("Luana", 21, "Diretora de Escola", );
         Pessoa pessoa2 = new Pessoa("Carlos", 31, "Arquiteto","");
-        Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico","456473534-65");
-        Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada","324234255-75");
-        Pessoa pessoa5 = new Pessoa("Lucas", 23, "Estagiario de direito","43144353-54");
+        Pessoa pessoa3 = new Pessoa("Jese", 71, "Medico",);
+        Pessoa pessoa4 = new Pessoa("Adriana", 53, "Advogada",);
+        Pessoa pessoa5 = new Pessoa("Lucas", 23, "Estagiario de direito",);
 
 
         ArrayList<Pessoa> pessoas = new ArrayList<>();
