9fdb872a4e242197465c15c534b01747d856d36e
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-28 21:34:27 -0300

    adicionado cpf nas pessoas

--- src/ListaJava/dominio/Pessoa.java
+++ src/ListaJava/dominio/Pessoa.java
@@ -4,12 +4,13 @@ public class Pessoa {
     public String nome;
     public int idade;
     public String trabalho;
+    public String cpf;
 
-
-    public Pessoa(String nome, int idade, String trabalho) {
+    public Pessoa(String nome, int idade, String trabalho, String cpf) {
         this.nome = nome;
         this.idade = idade;
         this.trabalho = trabalho;
+        this.cpf = cpf;
     }
 
     @Override
@@ -18,6 +19,7 @@ public class Pessoa {
                 "nome='" + nome + '\'' +
                 ", idade=" + idade +
                 ", trabalho='" + trabalho + '\'' +
+                ", cpf='" + cpf + '\'' +
                 '}';
     }
 }
