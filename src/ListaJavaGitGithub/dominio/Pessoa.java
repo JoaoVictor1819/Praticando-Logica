30df2d814f2a1950bbe30b40c82e153cbd683457
João Victor <victorlopesdemedeirosjoao@gmail.com>
2026-07-29 22:12:05 -0300

    ajustando codigo e arquivo

--- src/ListaJavaGitGithub/dominio/Pessoa.java
+++ src/ListaJavaGitGithub/dominio/Pessoa.java
@@ -1,17 +1,17 @@
-package ListaJava.dominio;
+package ListaJavaGitGithub.dominio;
 
 public class Pessoa {
     public String nome;
     public int idade;
     public String trabalho;
-    private double salario;
+   // private double salario;
 
 
-    public Pessoa(String nome, int idade, String trabalho, double salario) {
+    public Pessoa(String nome, int idade, String trabalho) {
         this.nome = nome;
         this.idade = idade;
         this.trabalho = trabalho;
-        this.salario = salario;
+      //  this.salario = salario;
     }
 
 
@@ -21,7 +21,7 @@ public class Pessoa {
                 "nome='" + nome + '\'' +
                 ", idade=" + idade +
                 ", trabalho='" + trabalho + '\'' +
-                ", salario=" + salario +
                 '}';
     }
 }
+
